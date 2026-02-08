7707a3d4de899b376b3bcbe87faa8d3f6e9a51a0
sdhar25 <dhar.shreya253@gmail.com>
2026-02-08 16:03:29 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Maincard from './components/Maincard';
+
+
+
+function App() {
+  
+
+  return (
+    <>
+        <Maincard />
+    </>
+  )
+}
+
+export default App
